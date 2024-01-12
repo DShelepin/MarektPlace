d0df167e81929e98bfdb47752a700a2209c8d08f
DShelepin <daniilselepin73@gmail.com>
2024-01-12 14:49:18 +0300

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Categories } from './pages/Categories';
+import { Products } from './pages/Products';
+import { AdminLayout } from './components/AdminLayout/AdminLayout';
 import './styles/globals.scss';
-import { Sidebar } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-    </div>
+    <Routes>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={<Navigate to="/admin/categories" replace />}
+        />
+        <Route path="/admin/categories" element={<Categories />} />
+        <Route path="/admin/products" element={<Products />} />
+        <Route path="*" element={<Navigate to="/admin/categories" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
